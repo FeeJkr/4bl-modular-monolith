7e4418d1c5f0556addcfa02018bbb87529fd09c3
Sergii Siryi <sergii.siryi97@gmail.com>
2020-09-19 19:45:51 +0200

    prepare modular architecture

--- src/UI/Web/Action/GenerateJWTTokenAction.php
+++ src/UI/Web/Action/GenerateJWTTokenAction.php
@@ -1,23 +1,22 @@
 <?php
 declare(strict_types=1);
 
-namespace App\UI\Web\Controller;
+namespace App\UI\Web\Action;
 
 use Firebase\JWT\JWT;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
-final class IndexController extends AbstractController
+final class GenerateJWTTokenAction extends Action
 {
-    private $jwtSecretKey;
+    private string $jwtSecretKey;
 
     public function __construct(string $jwtSecretKey)
     {
         $this->jwtSecretKey = $jwtSecretKey;
     }
 
-    public function generateJWT(Request $request): Response
+    public function __invoke(Request $request): JsonResponse
     {
         $payload = [
             'exp' => (new \DateTime())->modify('+ 30 days')->getTimestamp(),
