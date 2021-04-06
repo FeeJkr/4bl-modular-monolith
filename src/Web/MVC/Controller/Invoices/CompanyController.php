27906504080a910ca5412b013ce4517d1c11f3ae
Sergii Siryi <ssiryi@getresponse.com>
2021-04-06 23:02:53 +0200

    remove unused routes

--- src/Web/MVC/Controller/Invoices/CompanyController.php
+++ src/Web/MVC/Controller/Invoices/CompanyController.php
@@ -19,85 +19,35 @@ use Throwable;
 
 class CompanyController extends AbstractController
 {
-    public function __construct(private MessageBusInterface $bus){}
+	public function __construct(private MessageBusInterface $bus)
+	{
+	}
 
-    public function showCompaniesPage(): Response
-    {
-        /** @var CompaniesCollection $companies */
-        $companies = $this->bus->dispatch(new GetAllCompaniesQuery())->last(HandledStamp::class)->getResult();
+	public function list(): Response
+	{
+		/** @var CompaniesCollection $companies */
+		$companies = $this->bus->dispatch(new GetAllCompaniesQuery())->last(HandledStamp::class)->getResult();
 
-        return $this->render('invoices/company/index.html.twig', [
-            'companies' => $companies->toArray(),
-        ]);
-    }
+		return $this->render('invoices/company/index.html.twig', [
+			'companies' => $companies->toArray(),
+		]);
+	}
 
-    public function showCreateCompanyPage(): Response
-    {
-        return $this->render('invoices/company/create.html.twig');
-    }
+	public function create(): Response
+	{
+		return $this->render('invoices/company/create.html.twig');
+	}
 
-    public function showEditCompanyPage(Request $request): Response
-    {
-        /** @var CompanyDTO $company */
-        $company = $this->bus
-            ->dispatch(new GetOneCompanyByIdQuery((int) $request->get('id')))
-            ->last(HandledStamp::class)
-            ->getResult();
+	public function edit(Request $request): Response
+	{
+		/** @var CompanyDTO $company */
+		$company = $this->bus
+			->dispatch(new GetOneCompanyByIdQuery((int)$request->get('id')))
+			->last(HandledStamp::class)
+			->getResult();
 
-        return $this->render('invoices/company/edit.html.twig', [
-            'company' => $company,
-        ]);
-    }
-
-    public function editCompany(Request $request): Response
-    {
-        $id = (int) $request->get('id');
-        $name = $request->get('name');
-        $identificationNumber = $request->get('identificationNumber');
-        $email = $request->get('email');
-        $phoneNumber = $request->get('phoneNumber');
-        $paymentType = $request->get('paymentType');
-        $paymentLastDate = $request->get('paymentLastDate');
-        $bank = $request->get('bank');
-        $accountNumber = $request->get('accountNumber');
-        $street = $request->get('street');
-        $zipCode = $request->get('zipCode');
-        $city = $request->get('city');
-
-        Assert::lazy()
-            ->that($id, 'id')->notEmpty()->numeric()
-            ->that($name, 'name')->notEmpty()
-            ->that($identificationNumber, 'identificationNumber')->notEmpty()
-            ->that($email, 'email')->nullOr()->email()
-            ->that($phoneNumber, 'phoneNumber')->nullOr()->startsWith('+')
-            ->that($paymentType, 'paymentType')->nullOr()->notEmpty()
-            ->that($paymentLastDate, 'paymentLastDate')->nullOr()->numeric()
-            ->that($bank, 'bank')->nullOr()->notEmpty()
-            ->that($accountNumber, 'accountNumber')->nullOr()->notEmpty()
-            ->that($street, 'street')->notEmpty()
-            ->that($zipCode, 'zipCode')->notEmpty()
-            ->that($city, 'city')->notEmpty()
-            ->verifyNow();
-
-        $this->bus->dispatch(
-            new UpdateCompanyPaymentInformationCommand(
-                $id,
-                $street,
-                $zipCode,
-                $city,
-                $name,
-                $identificationNumber,
-                $email,
-                $phoneNumber,
-                $paymentType,
-                $paymentLastDate,
-                $bank,
-                $accountNumber
-            )
-        );
-
-        $this->addFlash('success', 'Company was successfully updated.');
-
-        return $this->json(['status' => 'success']);
-    }
+		return $this->render('invoices/company/edit.html.twig', [
+			'company' => $company,
+		]);
+	}
 }
